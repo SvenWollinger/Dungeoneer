275266c402291360e96cda4185ad50c936ea73ed
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-11-18 13:45:58 +0100

    Optimized FileUtils.backupIncrement function

--- src/main/java/io/wollinger/dungeoneer/utils/FileUtils.java
+++ src/main/java/io/wollinger/dungeoneer/utils/FileUtils.java
@@ -5,25 +5,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class FileUtils {
+    private static final int backupsToKeep = 15;
 
     // This method copies our current db file to the backups folder and renames it, always keeping the latest 5 backups alive.
     // This should be called BEFORE saving, so that a new fresh db file is created in the currents place
     public static void backupIncrement(String serverID) {
         new File("backups").mkdir();
         File currentDB = new File(StringUtils.format("%c.db", serverID));
-        File backupFile1 = new File(StringUtils.format("backups/%c.db.1", serverID));
-        File backupFile2 = new File(StringUtils.format("backups/%c.db.2", serverID));
-        File backupFile3 = new File(StringUtils.format("backups/%c.db.3", serverID));
 
-        if(backupFile3.exists())
-            backupFile3.delete();
-        if(backupFile2.exists())
-            backupFile2.renameTo(backupFile3);
-        if(backupFile1.exists())
-            backupFile1.renameTo(backupFile2);
+        File[] backups = new File[backupsToKeep];
+        for(int i = backupsToKeep-1; i >= 0; i--) {
+            backups[i] = new File(StringUtils.format("backups/%c.db.%c", serverID, i));
+            if (i == backupsToKeep - 1) {
+                if(backups[i].exists())
+                    backups[i].delete();
+            } else {
+                backups[i].renameTo(backups[i+1]);
+            }
+        }
 
         try {
-            Files.copy(currentDB.toPath(), backupFile1.toPath());
+            Files.copy(currentDB.toPath(), backups[0].toPath());
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
