db44b7f843f57bfc5ceba13c87a5b383c1e0d662
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-11-22 09:47:01 +0100

    Create ServerConfig.java

---
+++ src/main/java/io/wollinger/dungeoneer/server/ServerConfig.java
@@ -0,0 +1,13 @@
+package io.wollinger.dungeoneer.server;
+
+public class ServerConfig {
+    private String commandPrefix = "!";
+
+    public ServerConfig() {
+
+    }
+
+    public String getCommandPrefix() {
+        return commandPrefix;
+    }
+}
