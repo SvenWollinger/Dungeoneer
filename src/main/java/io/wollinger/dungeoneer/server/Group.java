ad1e120ef3192bc98f8d1ae851851276ab806786
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-11-22 08:20:34 +0100

    Added more fields to Group

--- src/main/java/io/wollinger/dungeoneer/server/Group.java
+++ src/main/java/io/wollinger/dungeoneer/server/Group.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class Group {
     private String name;
+    private String description;
+    private long timeUnix;
+    private boolean repeat;
     private GroupWebhook webhook;
-    private ArrayList<GroupMember> members = new ArrayList<>();
+    private final ArrayList<GroupMember> members = new ArrayList<>();
 }
